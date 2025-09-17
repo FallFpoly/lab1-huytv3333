dca39dd24065770c4d570fca531fab44e62f97d9
huytv3333 <huytv05102780@gmail.com>
2025-09-17 21:53:38 +0700

    nộp lab 1

--- Lab1/src/main/java/com/mycompany/lab1/bai2.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai2.java
@@ -12,5 +12,7 @@ public class bai2 {
 
     public static void main(String[] args) {
           // sinh viên code ở đây
+        System.out.println("lam lab 1 bai o day");
+        System.out.println("Sinh Vien Ngueyn Van A");
     }
 }
